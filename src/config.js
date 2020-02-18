78f1d8b31239b79866a58ab367b8aa93c9794d3e
Artur Manukian <manukian.artur.94@gmail.com>
2020-02-18 23:19:05 +0200

    feature: add TOKEN_PROD to config

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 export default {
   APP_PORT: process.env.APP_PORT,
   TOKEN_DEV: process.env.TOKEN_DEV,
+  TOKEN_PROD: process.env.TOKEN_PROD,
   WEBHOOK_PATH: process.env.WEBHOOK_PATH,
   WEBHOOK_PORT: process.env.WEBHOOK_PORT,
   WEBHOOK_URL: process.env.WEBHOOK_URL,
