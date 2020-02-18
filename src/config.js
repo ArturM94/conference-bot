db2eb7a52001d8ce10d989d072dfa7ab982416f8
Artur Manukian <manukian.artur.94@gmail.com>
2020-02-18 18:06:25 +0200

    feature: add configs for bot and webhook

---
+++ src/config.js
@@ -0,0 +1,6 @@
+export default {
+  APP_PORT: process.env.APP_PORT,
+  TOKEN_DEV: process.env.TOKEN_DEV,
+  WEBHOOK_PATH: process.env.WEBHOOK_PATH,
+  WEBHOOK_URL_DEV: process.env.WEBHOOK_URL_DEV,
+};
