9ef370db60065d2c13e6270a40c76e6404e049f9
Artur Manukian <manukian.artur.94@gmail.com>
2020-02-18 18:10:09 +0200

    feature: add bot with express server

---
+++ src/serverBot.js
@@ -0,0 +1,52 @@
+import 'dotenv/config';
+import express from 'express';
+import Telegraf from 'telegraf';
+
+import config from './config';
+
+const {
+  TOKEN_DEV, WEBHOOK_PATH, WEBHOOK_URL_DEV, APP_PORT,
+} = config;
+
+const app = express();
+const telegraf = new Telegraf(TOKEN_DEV);
+
+const attachBotWebhook = async (bot, url, path) => {
+  try {
+    await bot.telegram.setWebhook(`${url}${path}`);
+
+    const webhookInfo = await bot.telegram.getWebhookInfo();
+    console.log('Webhook successfully attached\n', webhookInfo);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const attachBotHandlers = (bot) => {
+  bot.start((ctx) => ctx.reply('welcome!'));
+
+  bot.command('schedule', (ctx) => ctx.reply('schedule command'));
+  bot.command('organizers', (ctx) => ctx.reply('organizers command'));
+  bot.command('lunch', (ctx) => ctx.reply('lunch command'));
+  bot.command('add', (ctx) => ctx.reply('add notification command'));
+  bot.command('delete', (ctx) => ctx.reply('delete notification command'));
+
+  bot.on('text', (ctx) => ctx.reply(`hello, ${ctx.message.from.first_name}!`));
+  bot.on('sticker', (ctx) => ctx.reply('sticker echo'));
+  bot.on('message', (ctx) => ctx.reply('message echo'));
+
+  bot.catch((error, ctx) => {
+    console.log(`Ooops, encountered an error for ${ctx.updateType}`, error);
+  });
+};
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(telegraf.webhookCallback('/bot'));
+
+attachBotWebhook(telegraf, WEBHOOK_URL_DEV, WEBHOOK_PATH);
+attachBotHandlers(telegraf);
+
+app.listen(APP_PORT, () => {
+  console.log(`Bot listening on port ${APP_PORT}`);
+});
