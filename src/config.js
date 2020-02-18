9fa952643e8249a0550b90e0a68462b98e6b59a3
Artur Manukian <manukian.artur.94@gmail.com>
2020-02-18 22:27:47 +0200

    feature: add webhook port in config

--- src/config.js
+++ src/config.js
@@ -2,5 +2,6 @@ export default {
   APP_PORT: process.env.APP_PORT,
   TOKEN_DEV: process.env.TOKEN_DEV,
   WEBHOOK_PATH: process.env.WEBHOOK_PATH,
+  WEBHOOK_PORT: process.env.WEBHOOK_PORT,
   WEBHOOK_URL_DEV: process.env.WEBHOOK_URL_DEV,
 };
