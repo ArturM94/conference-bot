cc207fcc412ccb90a0dda6a8193591113356c433
Odavinh <penxerscky@gmail.com>
2020-02-18 12:58:40 +0200

    add speaker model

---
+++ src/database/models/speaker.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+module.exports = mongoose.model(
+  "speaker",
+  new mongoose.Schema({
+    first_name: {
+      type: String,
+      require: true
+    },
+    last_name: {
+      type: String,
+      require: true
+    },
+    position: {
+      type: String,
+      require: true
+    },
+    company: {
+      type: String,
+      require: false
+    },
+    country: {
+      type: String,
+      require: false
+    },
+    topic: {
+      type: String,
+      unique: true,
+      require: false
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now
+    }
+  })
+);
