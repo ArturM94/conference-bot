cd178c28610a8dc52748981771c7bedf058f9a5f
Odavinh <penxerscky@gmail.com>
2020-02-18 15:20:43 +0200

    feature: add shadule model

---
+++ src/database/models/schedule.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+module.exports = mongoose.model(
+  'schedule',
+  new mongoose.Schema({
+    date: {
+      type: String,
+      require: true
+    },
+    time: {
+      type: String,
+      require: true
+    },
+    flow: {
+      type: String,
+      enum: ['technical', 'non-technical'],
+      require: true
+    },
+    speakerId: {
+      type: mongoose.Types.ObjectId,
+      require: true
+    },
+    details: {
+      type: String
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now
+    }
+  })
+);
