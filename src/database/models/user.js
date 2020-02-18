373bc9246e56a8175ef6644d94bd27b8385b0abb
Odavinh <penxerscky@gmail.com>
2020-02-18 12:21:05 +0200

    feature: add user model

---
+++ src/database/models/user.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+module.exports = mongoose.model(
+  'user',
+  new mongoose.Schema({
+    first_name: {
+      type: String,
+      require: true
+    },
+    last_name: {
+      type: String,
+      require: true
+    },
+    phoneNumber: {
+      type: String,
+      unique: true,
+      require: true,
+      maxlength: 13,
+      minlength: 9
+    },
+    chatId: {
+      type: Number,
+      require: true
+    },
+    isAdmin: {
+      type: Boolean,
+      require: true,
+      default: false
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now
+    }
+  })
+);
