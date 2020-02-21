be2f0abd2d6800bcacbc5c7ac28d3a5343a6cdb5
Volodymyr <klischvolodimir@gmail.com>
2020-02-21 18:48:09 +0200

    feature: changed winston.add(...)

--- src/helpers/logger.js
+++ src/helpers/logger.js
@@ -1,6 +1,10 @@
 const winston = require('winston');
 const CloudWatchTransport = require('winston-aws-cloudwatch');
-require('dotenv').config();
+const config = require('./config');
+
+const {
+  NODE_ENV, AWS_ACCESS_KEY, AWS_SECRET_ACCESS_KEY, AWS_REGION,
+} = config;
 
 const logger = winston.createLogger({
   transports: [
@@ -13,17 +17,17 @@ const logger = winston.createLogger({
 
 const cloudwatchConfig = {
   logGroupName: 'conference-telegram-bot',
-  logStreamName: process.env.NODE_ENV,
+  logStreamName: NODE_ENV,
   createLogGroup: true,
   createLogStream: true,
   awsConfig: {
-    accessKeyId: process.env.AWS_ACCESS_KEY,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    region: process.env.AWS_REGION,
+    accessKeyId: AWS_ACCESS_KEY,
+    secretAccessKey: AWS_SECRET_ACCESS_KEY,
+    region: AWS_REGION,
   },
   formatLog: (item) => `${item.level}: ${item.message} ${JSON.stringify(item.meta)}`,
 };
 
-logger.add(CloudWatchTransport, cloudwatchConfig);
+logger.add(new CloudWatchTransport(cloudwatchConfig));
 
 module.exports = logger;
