719e6c2436c31ca3f77763fc3cca8cec8f699bb3
Volodymyr <klischvolodimir@gmail.com>
2020-02-24 18:04:38 +0200

    feature: tested husky

--- src/handler.js
+++ src/handler.js
@@ -2,5 +2,5 @@
 const logger = require('./helpers/logger');
 
 module.exports.logger = async () => {
-  logger.info('hello', { message: 'world' });
+  logger.info('hello 🔥 ', { message: 'world' });
 };
