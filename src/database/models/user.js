a1aea7e327239ca00524738b814520540db0ee84
Volodymyr <klischvolodimir@gmail.com>
2020-02-24 18:00:24 +0200

    featere: merged husky branch

---
+++ src/database/models/user.js
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose';
+
+export default mongoose.model(
+  'user',
+  new mongoose.Schema({
+    firstName: {
+      type: String,
+      require: true,
+    },
+    lastName: {
+      type: String,
+      require: true,
+    },
+    phoneNumber: {
+      type: String,
+      unique: true,
+      require: true,
+      maxlength: 13,
+      minlength: 9,
+    },
+    chatId: {
+      type: Number,
+      require: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      require: true,
+      default: false,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  }),
+);
