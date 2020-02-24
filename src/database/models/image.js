ffc21b216d2a2d73fdb88d151bb7a512835a1732
Odavinh <penxerscky@gmail.com>
2020-02-24 14:28:24 +0200

    fix: replaced createdAt by timestamps

--- src/database/models/image.js
+++ src/database/models/image.js
@@ -2,18 +2,19 @@ import mongoose from 'mongoose';
 
 export default mongoose.model(
   'image',
-  new mongoose.Schema({
-    owner: {
-      type: String,
-      require: true,
+  new mongoose.Schema(
+    {
+      owner: {
+        type: String,
+        require: true,
+      },
+      imageUrl: {
+        type: String,
+        require: true,
+      },
     },
-    imageUrl: {
-      type: String,
-      require: true,
-    },
-    createdAt: {
-      type: Date,
-      default: Date.now,
-    },
-  }),
+    {
+      timestamps: true,
+    }
+  )
 );
