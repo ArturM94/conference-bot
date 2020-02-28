bc97b9c35a20b524586a2922a4a78ae70681cb4e
Volodymyr <klischvolodimir@gmail.com>
2020-02-28 16:41:41 +0200

    fix: fixed problem with parsing photo

--- src/bot/commandHandlers/lunchHandler.js
+++ src/bot/commandHandlers/lunchHandler.js
@@ -1,9 +1,16 @@
 const { getScheduleByDetails } = require('../../database/wrappers/schedule');
 const { getImagesByOwnerId } = require('./../../database/wrappers/image');
+const logger = require('../../helpers/logger');
 
 // command '/lunch'
-export default async (ctx) => {
-  const image = await getImagesByOwnerId(ctx.chat.id);
-  const shedule = await getScheduleByDetails('example');
-  await ctx.replyWithPhoto(`${image.imageUrl}`, { caption: `${shedule.date}` });
+module.exports = async (ctx) => {
+  try {
+    const image = await getImagesByOwnerId(ctx.chat.id);
+    console.log(image);
+    const shedule = await getScheduleByDetails('example');
+    await ctx.replyWithPhoto(`${image[0].imageUrl}`, { caption: `${shedule[0].date}` });
+  } catch (e) {
+    logger.error(e);
+    console.log(e);
+  }
 };
