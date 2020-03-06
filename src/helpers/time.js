2f59b103d72d17510caa20475f8d6819be3964b0
Volodymyr <klischvolodimir@gmail.com>
2020-03-06 14:57:11 +0200

    fix: fixed problem with GMT+2

--- src/helpers/time.js
+++ src/helpers/time.js
@@ -16,7 +16,7 @@ const getTimeFromMsg = (obj) => {
   const a = obj.split(' ');
   let hour = a[2];
   hour = hour.split(':');
-  hour[0] = +hour[0] + 3;
+  hour[0] = +hour[0] + 2;
   hour = hour.join(':');
   return new Date(`${new Date().getFullYear()} ${a[0]} ${a[1]} ${hour}`);
 };
